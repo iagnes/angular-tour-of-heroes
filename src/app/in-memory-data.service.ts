ebab1269ff6f3a27d3d9c9f879ec3e9c3ae3b190
Roxana Gnes <roxana.gnes@stud.hslu.ch>
2021-08-07 11:51:49 +0200

    Step 6 completed
    Tutorial Step 6. Get Data from Server completed

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from './hero';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes = [
+      { id: 11, name: 'Dr. Nice' },
+      { id: 12, name: 'Frauenarzt Testo' },
+      { id: 13, name: 'Luani69' },
+      { id: 14, name: 'Dr. Gnesitto' },
+      { id: 15, name: 'Dr. med. gyn. Cusseddu' },
+      { id: 16, name: 'Doc. Elio' },
+      { id: 17, name: 'Mrs. Dynamite' },
+      { id: 18, name: 'Dr. med. prof. liq. jur. Herkules' },
+      { id: 19, name: 'Ms. Tornadini' },
+      { id: 20, name: 'Dr. Hello World' }
+    ];
+    return {heroes};
+  }
+
+  // Overrides the genId method to ensure that a hero always has an id.
+  // If the heroes array is empty,
+  // the method below returns the initial number (11).
+  // if the heroes array is not empty, the method below returns the highest
+  // hero id + 1.
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+  }
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr. Nice' },
+  { id: 12, name: 'Frauenarzt Testo' },
+  { id: 13, name: 'Luani69' },
+  { id: 14, name: 'Dr. Gnesitto' },
+  { id: 15, name: 'Dr. med. gyn. Cusseddu' },
+  { id: 16, name: 'Doc. Elio' },
+  { id: 17, name: 'Mrs. Dynamite' },
+  { id: 18, name: 'Dr. med. prof. liq. jur. Herkules' },
+  { id: 19, name: 'Ms. Tornadini' },
+  { id: 20, name: 'Dr. Hello World' }
+];
